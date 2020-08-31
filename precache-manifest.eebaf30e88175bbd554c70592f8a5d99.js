ee4d10fad9f4e55025b9b7a5e2e782b932ad33a3
Dean Krotzer <deankrotzer@Deans-MacBook-Pro.local>
2020-08-31 12:38:58 -0700

    Updates

---
+++ precache-manifest.eebaf30e88175bbd554c70592f8a5d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3efc95ac316e1fd613c11e2d2bc686a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8610affdb4fb8ee875",
+    "url": "/static/css/main.b5c1cae5.chunk.css"
+  },
+  {
+    "revision": "80c07f1d6eec41d7438d",
+    "url": "/static/js/2.6c789bbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6c789bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8610affdb4fb8ee875",
+    "url": "/static/js/main.bbfa40dc.chunk.js"
+  },
+  {
+    "revision": "39d9feb74b916bb861d0",
+    "url": "/static/js/runtime-main.901c936d.js"
+  },
+  {
+    "revision": "89ae5b550436c4a1023477b457a109c4",
+    "url": "/static/media/PortfolioPic.89ae5b55.png"
+  }
+]);
