58c681fc2b6786166c7b383db57c923f0fb8d482
Dean Krotzer <deankrotzer@Deans-MacBook-Pro.local>
2020-08-31 14:03:35 -0700

    Updates

---
+++ precache-manifest.3d928729efdde386f7b70cc745579979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb7f9263a99f578a32c7b43a62cbc71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7a9e1ff8233f5c541da",
+    "url": "./static/css/main.b5c1cae5.chunk.css"
+  },
+  {
+    "revision": "80c07f1d6eec41d7438d",
+    "url": "./static/js/2.6c789bbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.6c789bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a9e1ff8233f5c541da",
+    "url": "./static/js/main.946fe938.chunk.js"
+  },
+  {
+    "revision": "0e253a5ef9556704b2e1",
+    "url": "./static/js/runtime-main.ddb83dd4.js"
+  },
+  {
+    "revision": "89ae5b550436c4a1023477b457a109c4",
+    "url": "./static/media/PortfolioPic.89ae5b55.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eebaf30e88175bbd554c70592f8a5d99.js"
+  "./precache-manifest.3d928729efdde386f7b70cc745579979.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
