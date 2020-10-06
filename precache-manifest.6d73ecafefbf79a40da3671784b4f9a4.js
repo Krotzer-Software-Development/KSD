f7a4d6ed8858f9d8fc2a969f50890c3410101bcd
Dean Krotzer <krotzersoftwaredevelopment@gmail.com>
2020-10-05 21:50:09 -0700

    Updates

---
+++ precache-manifest.6d73ecafefbf79a40da3671784b4f9a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab6e5bb08b2f87fbdc3e4d5088f38ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202e28b123bfb842c7c1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a3bc91ff831e86de82de",
+    "url": "/static/css/main.e47b0901.chunk.css"
+  },
+  {
+    "revision": "202e28b123bfb842c7c1",
+    "url": "/static/js/2.5533144e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5533144e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bc91ff831e86de82de",
+    "url": "/static/js/main.c1cef2bc.chunk.js"
+  },
+  {
+    "revision": "633f389bac66d474f8c6",
+    "url": "/static/js/runtime-main.fd73fb97.js"
+  },
+  {
+    "revision": "7c5e902de551bd7f0e8af46950747289",
+    "url": "/static/media/bg.7c5e902d.png"
+  }
+]);
