3da25d8ffad877cbb3b12509138b00d1e6663b5e
Dean Krotzer <krotzersoftwaredevelopment@gmail.com>
2020-10-20 16:39:42 -0700

    Updates

---
+++ precache-manifest.ad20cb1575031c97f7023b32fd1d52e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4d442839ae0180121f160d923a3353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f3365ff526e18c0925",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ccae27f92cca0a2d5a36",
+    "url": "/static/css/main.0fd3d011.chunk.css"
+  },
+  {
+    "revision": "00f3365ff526e18c0925",
+    "url": "/static/js/2.ff45aba5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ff45aba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccae27f92cca0a2d5a36",
+    "url": "/static/js/main.d855b0dd.chunk.js"
+  },
+  {
+    "revision": "633f389bac66d474f8c6",
+    "url": "/static/js/runtime-main.fd73fb97.js"
+  },
+  {
+    "revision": "a5e1da9b16f99ed2fed4d351515caa24",
+    "url": "/static/media/bg.a5e1da9b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.48ada0c9dbfff407b5a05ca48aee4336.js"
+  "/precache-manifest.ad20cb1575031c97f7023b32fd1d52e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
