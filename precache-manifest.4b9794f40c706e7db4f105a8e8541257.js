c251ad80459d6ede3b0d67160fecbb371b434de8
Dean Krotzer <krotzersoftwaredevelopment@gmail.com>
2020-10-20 16:59:35 -0700

    Updates

---
+++ precache-manifest.4b9794f40c706e7db4f105a8e8541257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1487164d24c19514c9a2161cec0017ae",
+    "url": "/ksd/index.html"
+  },
+  {
+    "revision": "00f3365ff526e18c0925",
+    "url": "/ksd/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2e0617d976f0153b5fcb",
+    "url": "/ksd/static/css/main.0fd3d011.chunk.css"
+  },
+  {
+    "revision": "00f3365ff526e18c0925",
+    "url": "/ksd/static/js/2.ff45aba5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ksd/static/js/2.ff45aba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0617d976f0153b5fcb",
+    "url": "/ksd/static/js/main.3fac2976.chunk.js"
+  },
+  {
+    "revision": "6e2a513e27cddebe7e01",
+    "url": "/ksd/static/js/runtime-main.1c88ed76.js"
+  },
+  {
+    "revision": "a5e1da9b16f99ed2fed4d351515caa24",
+    "url": "/ksd/static/media/bg.a5e1da9b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad20cb1575031c97f7023b32fd1d52e4.js"
+  "/ksd/precache-manifest.4b9794f40c706e7db4f105a8e8541257.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ksd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
